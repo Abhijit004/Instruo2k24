1e7df30ea947a680b12d957423fdda3b3ed7c8e4
Saral <saral.guptax@gmail.com>
2024-09-23 02:54:05 +0530

    update footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -8,7 +8,7 @@ import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <footer className="bg-black text-white py-6 w-full">
+    <footer className="bg-black text-white py-6 w-full border-t border-gray-100/10">
       <div className="container mx-auto px-4">
         <div className="flex flex-col md:flex-row justify-between items-center">
           <div className="mb-4 md:mb-0">
