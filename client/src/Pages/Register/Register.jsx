7634d86541c103fe734224d9c21d237bd6f5341f
Soumyajit <sona832004@gmail.com>
2024-11-03 13:54:51 +0530

    [UPDATE]: Adding backgroundBeams to register page

--- client/src/Pages/Register/Register.jsx
+++ client/src/Pages/Register/Register.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
-import "./Register.css"
-import EventRegistration from '../../components/EventRegistration/EventRegistration'
+import React from "react";
+import "./Register.css";
+import EventRegistration from "../../components/EventRegistration/EventRegistration";
 import { BackgroundBeams } from "../../components/ui/background-beams";
 
 const Register = () => {
-  return (
-    <div className="register-wrapper">
-        <BackgroundBeams />
-        <EventRegistration />
-    </div>
-  )
-}
+    return (
+        <div className="register-wrapper">
+            <div className="fixed-ui">
+                <BackgroundBeams />
+            </div>
+            <EventRegistration />
+        </div>
+    );
+};
 
-export default Register
+export default Register;
